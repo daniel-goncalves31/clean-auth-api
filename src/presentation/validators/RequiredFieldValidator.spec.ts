bf58ba8f2ae7ce2c4a12374ec5fb674f79934e0e
Daniel Gonçalves <danielgg0831@gmail.com>
2020-08-01 16:02:08 -0300

    test: returns null if the required field is provided

--- src/presentation/validators/RequiredFieldValidator.spec.ts
+++ src/presentation/validators/RequiredFieldValidator.spec.ts
@@ -18,14 +18,23 @@ describe('RequiredFieldValidator', () => {
   test('should return an MissingParamError if an field is not provided', () => {
     const { sut } = makeSut()
 
-    const fakeHttpRequest: HttpRequest = {
-      body: {
-        field1: 'any_value',
-        field3: 'any_value'
-      }
+    const input = {
+      field1: 'any_value',
+      field3: 'any_value'
     }
 
-    const response = sut.validate(fakeHttpRequest)
+    const response = sut.validate(input)
     expect(response).toEqual(new MissingParamError('field2'))
+  })
+  test('should return null if the required field is provided', () => {
+    const { sut } = makeSut()
+
+    const input = {
+      field1: 'any_value',
+      field2: 'any_value'
+    }
+
+    const response = sut.validate(input)
+    expect(response).toBeNull()
   })
 })
