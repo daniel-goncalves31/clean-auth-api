129104208a1f13a101ea97449d16895fb439d320
Daniel Gonçalves <danielgg0831@gmail.com>
2020-08-01 20:44:23 -0300

    feat(SignUpController): call Validator with correct values

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -1,28 +1,12 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
-import { badRequest } from '../protocols/http-responses'
-import { MissingParamError, InvalidParamError } from '../protocols/errors'
 import { Controller } from '../protocols/Controller'
+import { Validator } from '../protocols/Validator'
 
 export class SignUpController implements Controller {
+  constructor (private readonly validator: Validator) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { body } = httpRequest
-
-    if (!body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-
-    if (!body.email) {
-      return badRequest(new MissingParamError('email'))
-    }
-
-    if (!body.password) {
-      return badRequest(new MissingParamError('password'))
-    }
-
-    if (!body.confirmPassword) {
-      return badRequest(new MissingParamError('confirmPassword'))
-    }
-
-    return badRequest(new InvalidParamError('confirmPassword'))
+    this.validator.validate(httpRequest.body)
+    return {} as any
   }
 }
---
+++ src/presentation/validators/ValidatorComposite.ts
@@ -0,0 +1,15 @@
+import { Validator } from '../protocols/Validator'
+
+export class ValidatorComposite implements Validator {
+  constructor (private readonly validators: Validator[]) {}
+
+  validate (input: any): Error | null {
+    for (const validator of this.validators) {
+      const error = validator.validate(input)
+
+      if (error) return error
+    }
+
+    return null
+  }
+}
