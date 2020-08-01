cf7723914f429f35b8780dd5d790429d266c3844
Daniel Gonçalves <danielgg0831@gmail.com>
2020-08-01 13:24:10 -0300

    test: return 400 if no password is provided

--- src/presentation/controllers/SignUpController.spec.ts
+++ src/presentation/controllers/SignUpController.spec.ts
@@ -1,4 +1,6 @@
-import { SignUpController } from '@/presentation/controllers/SignUpController'
+import { SignUpController } from './SignUpController'
+import { HttpRequest } from '../protocols/http'
+import { MissingParamError } from '../protocols/errors/MissingParamError'
 
 interface SutType {
   sut: SignUpController
@@ -15,9 +17,22 @@ const makeSut = (): SutType => {
 describe('SignUpController', () => {
   test('should return 400 if no email is provided', () => {
     const { sut } = makeSut()
-    const httpRequest = { body: {} }
+    const httpRequest: HttpRequest = { body: {} }
 
     const httpResponse = sut.handle(httpRequest)
-    expect(httpResponse).toEqual({ status: 400 })
+    expect(httpResponse).toEqual({
+      status: 400,
+      body: new MissingParamError('email')
+    })
+  })
+  test('should return 400 if no password is provided', () => {
+    const { sut } = makeSut()
+    const httpRequest: HttpRequest = { body: { email: 'any_email@email.com' } }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse).toEqual({
+      status: 400,
+      body: new MissingParamError('email')
+    })
   })
 })
