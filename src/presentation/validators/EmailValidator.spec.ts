3aa42e3f4727c138f8936d8e77c6ad432eb630b5
Daniel Gonçalves <danielgg0831@gmail.com>
2020-08-01 23:13:01 -0300

    test: returns an error if the email is invalid

---
+++ src/presentation/validators/EmailValidator.spec.ts
@@ -0,0 +1,25 @@
+import { EmailValidator } from './EmailValidator'
+import { InvalidParamError } from '../protocols/errors'
+
+interface SutType {
+  sut: EmailValidator
+}
+
+const makeSut = (): SutType => {
+  const sut = new EmailValidator('email')
+
+  return {
+    sut
+  }
+}
+
+describe('EmailValidator', () => {
+  test('should return InvalidParamError if the email is not valid', () => {
+    const { sut } = makeSut()
+    const input = {
+      email: 'invalid_email'
+    }
+    const res = sut.validate(input)
+    expect(res).toEqual(new InvalidParamError('email'))
+  })
+})
